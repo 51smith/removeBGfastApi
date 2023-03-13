f51e3a13356d1e4aba360a2f04d56c6e71d6e213
Shane Smith <shane.smith@skyteam.com>
2023-03-13 11:07:33 +0100

    add extra background images add backgrounds api change background boolean to background name

---
+++ application/components/modnet_bg_remove.py
@@ -0,0 +1,181 @@
+import os
+import sys
+import copy
+import warnings
+
+import numpy as np
+import cv2
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+
+from application.components.models.modnet import MODNet
+
+
+warnings.filterwarnings("ignore")
+
+
+class MODNetBGRemove():
+    # define hyper-parameters
+    ref_size = 512
+
+    # define image to tensor transform
+    im_transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ]
+    )
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # create MODNet and load the pre-trained ckpt
+    modnet = MODNet(backbone_pretrained=False)
+    modnet = nn.DataParallel(modnet)
+    if device == 'cuda':
+        modnet = modnet.cuda()
+
+    def __init__(self, ckpt_path):
+        self.parameter_load(ckpt_path)
+
+    def parameter_load(self, ckpt_path):
+        MODNetBGRemove.modnet.load_state_dict(
+            torch.load(ckpt_path, map_location=MODNetBGRemove.device))
+        MODNetBGRemove.modnet.eval()
+
+    def file_load(self, filename):
+        im = cv2.imread(filename)
+        im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+        if len(im.shape) == 2:
+            im = im[:, :, None]
+        if im.shape[2] == 1:
+            im = np.repeat(im, 3, axis=2)
+        elif im.shape[2] == 4:
+            im = im[:, :, 0:3]
+
+        return im
+
+    def dir_check(self, path):
+        os.makedirs(path, exist_ok=True)
+        if not path.endswith('/'):
+            path += '/'
+        return path
+
+    def pre_process(self, im):
+        self.original_im = copy.deepcopy(im)
+
+        # convert image to PyTorch tensor
+        im = MODNetBGRemove.im_transform(im)
+
+        # add mini-batch dim
+        im = im[None, :, :, :]
+
+        # resize image for input
+        im_b, im_c, im_h, im_w = im.shape
+        self.height, self.width = im_h, im_w
+
+        if max(im_h, im_w) < MODNetBGRemove.ref_size or min(im_h, im_w) > MODNetBGRemove.ref_size:
+            if im_w >= im_h:
+                im_rh = MODNetBGRemove.ref_size
+                im_rw = int(im_w / im_h * MODNetBGRemove.ref_size)
+            elif im_w < im_h:
+                im_rw = MODNetBGRemove.ref_size
+                im_rh = int(im_h / im_w * MODNetBGRemove.ref_size)
+        else:
+            im_rh = im_h
+            im_rw = im_w
+
+        im_rw = im_rw - im_rw % 32
+        im_rh = im_rh - im_rh % 32
+        im = F.interpolate(im, size=(im_rh, im_rw), mode='area')
+        if MODNetBGRemove.device == 'cuda':
+            im = im.cuda()
+        return im
+
+    def post_process(self, mask_data, background="background.jpg", background_path='assets/background/'):
+        matte = F.interpolate(mask_data, size=(
+            self.height, self.width), mode='area')
+        matte = matte.repeat(1, 3, 1, 1)
+        matte = matte[0].data.cpu().numpy().transpose(1, 2, 0)
+        height, width, _ = matte.shape
+        print(background)
+
+        back_image = self.file_load(background_path+background)
+        print (back_image.size)
+        back_image = cv2.resize(
+            back_image, (width, height), cv2.INTER_AREA)
+
+        self.alpha = np.uint8(matte[:, :, 0]*255)
+
+        matte = matte * self.original_im + (1 - matte) * back_image
+        return matte
+
+    def image(self, filename, background="background.jpg", output='assets/output/', save=True):
+        output = self.dir_check(output)
+        print(background)
+        print("bg image "+filename)
+
+        self.im_name = filename.split('/')[-1]
+        im = self.file_load(filename)
+        im = self.pre_process(im)
+        _, _, matte = MODNetBGRemove.modnet(im, inference=False)
+        matte = self.post_process(matte, background)
+
+        if save:
+            matte = np.uint8(matte)
+            msg, name = self.save(matte, output)
+            return name
+        else:
+            h, w, _ = matte.shape
+            r_h, r_w = 720, int((w / h) * 720)
+            image = cv2.resize(self.original_im, (r_w, r_h), cv2.INTER_AREA)
+            matte = cv2.resize(matte, (r_w, r_h), cv2.INTER_AREA)
+
+            full_image = np.uint8(np.concatenate((image, matte), axis=1))
+            self.save(full_image, output)
+            exit_key = ord('q')
+            while True:
+                if cv2.waitKey(exit_key) & 255 == exit_key:
+                    cv2.destroyAllWindows()
+                    break
+                cv2.imshow(
+                    'MODNet - {} [Press "Q" To Exit]'.format(self.im_name), full_image)
+
+
+    def folder(self, foldername, background="background.jpg", output='output/'):
+        output = self.dir_check(output)
+        foldername = self.dir_check(foldername)
+
+        for filename in os.listdir(foldername):
+            try:
+                self.im_name = filename
+                im = self.file_load(foldername+filename)
+                im = self.pre_process(im)
+                _, _, matte = MODNetBGRemove.modnet(im, inference=False)
+                matte = self.post_process(matte, background)
+                status = self.save(matte, output)
+                print(status)
+            except:
+                print('There is an error for {} file/folder'.format(foldername+filename))
+
+    def save(self, matte, output_path='output/'):
+        extension = self.im_name.split('.')[-1]
+
+        if(extension == 'jpg' or extension == 'jpeg'):
+            name = '.'.join(self.im_name.split('.')[:-1])+'.png'
+        else:
+            name = self.im_name+'.png'
+
+        path = os.path.join(output_path, name)
+
+        print("saving new image "+name)
+
+
+        try:
+            matte = cv2.cvtColor(matte, cv2.COLOR_RGB2BGR)
+            cv2.imwrite(path, matte)
+            return "Successfully saved {}".format(path), name
+        except:
+            return "Error while saving {}".format(path), ''
+
--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-from io import BytesIO
+import json
 from typing import Optional
-
+import os
 from PIL import Image
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException, Path
 from io import BytesIO
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from werkzeug.utils import secure_filename
-from application.components.bg_remove import BGRemove
+from application.components.modnet_bg_remove import MODNetBGRemove
 import time
 import aiofiles
+import aiofiles.os
 import urllib
 
 from fastapi.middleware.cors import CORSMiddleware
 
+from dotenv import load_dotenv
 
+load_dotenv()
+
+#TODO set env vars in dockerfile & local
 
 app = FastAPI()
 app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 ckpt_image = 'application/pretrained/modnet_photographic_portrait_matting.ckpt'
-bg_remover = BGRemove(ckpt_image)
+MODNet_bg_remover = MODNetBGRemove(ckpt_image)
 
 app.add_middleware(
     CORSMiddleware,
@@ -31,7 +36,8 @@ app.add_middleware(
 
 class NFTItem(BaseModel):
     url: str
-    background: Optional[bool] = True
+    background: Optional[str] = "background.jpg"
+    model: Optional[str] = "MODNet"
 
 
 
@@ -39,59 +45,45 @@ class NFTItem(BaseModel):
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/get_backgrounds")
+async def get_backgrounds():
+    files = os.listdir(os.path.abspath("assets/background"))
+    filesList = [file for file in files if file.endswith(".jpg")]
+    jsonStr = json.dumps(filesList)
+    return jsonStr
+
+
 @app.post("/create_new_nft_image", status_code=201)
-async def create_file(nftItem: NFTItem ):
-    #async def test_function(dict: dict[str, str]):
+async def create_new_nft_image(nftItem: NFTItem ):
+
     # todo https://pypi.org/project/fastapi-easy-cache/
     background = nftItem.background
     file_url = nftItem.url
-    print("file_url = " + file_url)
 
     #Fixme: assuming arweave & extension is in the url als last query param
     extension = file_url.split("=")[-1]
-    if extension not in ("jpg", "jpeg", "png"):
-        print(extension)
-        raise HTTPException(status_code=422, detail="Image must be jpg or png format!")
+    if extension not in ("jpg", "jpeg", "png", "webp"):
+        extension="";#fixme, better check for filetype somewhere else
+    #    raise HTTPException(status_code=422, detail="Image must be jpg or png format!")
 
 
     filename = create_filename(file_url, extension)
-    print("filename = "+ filename)
-
     # todo check for network/http errors
     try:
         destination_file_path, headers = urllib.request.urlretrieve(file_url, "assets/input/" + filename)
         #todo switch models MODNet, DeepLabV3, for now just MODNet
-        output_filename = bg_remover.image(
+        output_filename = MODNet_bg_remover.image(
         destination_file_path, background=background, output="assets/output/", save=True)
-        print("output file" + output_filename)
+        print("output file " + output_filename)
+        return {"file_name": output_filename, "Result": "OK", "path": "assets/output/" + output_filename}
     except Exception as e:
         print(e)
 
     #todo return http error code
-    if not output_filename:
-        return "Something went wrong"
+    return HTTPException(status_code=500, detail="Something went wrong")
 
-    return {"file_name": filename, "Result": "OK", "path": "/assets/output/" + filename}
 
-@app.post("/upload-file", status_code=201)
-async def create_upload_file(background: bool = True, file: UploadFile = File(...)):
 
-    print("filename = ", file.filename) # getting filename
-    print(background)
-    extension = file.filename.split("=")[-1]
-    if extension not in ("jpg", "jpeg", "png"):
-        return "Image must be jpg or png format!"
-    filename = create_secure_filename(file)
-
-    destination_file_path = "assets/input/"+filename # location to store file
-    async with aiofiles.open(destination_file_path, 'wb') as out_file:
-        while content := await file.read(1024):  # async read file chunk
-            await out_file.write(content)  # async write file chunk
-
-    output_filename = bg_remover.image(
-        destination_file_path, background=background, output="assets/output/", save=True)
-
-    return {"file_name": file.filename, "Result": "OK", "path": "/assets/output/"+output_filename}
 
 def read_imagefile(file) -> Image.Image:
     image = Image.open(BytesIO(file))
@@ -106,24 +98,12 @@ def create_secure_filename(file):
 def create_filename(file_url: str, extension: str):
     url_path = file_url.split("/")[-1]
     file_name = url_path.split("?")[0]
+
     if file_name:
         ts = time.time()
-        filename = f"{str(ts)}-{file_name}.{extension}"
+        #remove decimal from float, to prevent error in bgremove.safe filename when there is no extension
+        filename = f"{str(ts).split('.')[0]}-{file_name}"
+        print(filename)
+        if extension:
+            filename = f"{filename}.{extension}"
         return secure_filename(filename)
-
-def download_photo(self, img_url, filename):
-    try:
-        image_on_web = urllib.urlopen(img_url)
-        if image_on_web.headers.maintype == 'image':
-            buf = image_on_web.read()
-            #path = os.getcwd() + DOWNLOADED_IMAGE_PATH
-            #file_path = "%s%s" % (path, filename)
-            #downloaded_image = file(file_path, "wb")
-            #downloaded_image.write(buf)
-            #downloaded_image.close()
-            image_on_web.close()
-        else:
-            return False
-    except:
-        return False
-    return True
